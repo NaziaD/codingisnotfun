93ffb4c7ce964e523d583841b1379cfc701d0905
NaziaD <nazdinath@gmail.com>
2019-09-21 17:34:44 -0500

    Initial commit.

---
+++ src/Banking_System.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class Banking_System {
+    public static void main(String[] args) {
+        System.out.println("Welcome to CIS-2348 Banking System!");
+
+    }//main end
+}//class end
