264318df73132a2f6a65f6236b41e50be209c548
NaziaD <nazdinath@gmail.com>
2019-10-03 16:57:44 -0500

    Finished Code 2.0

--- src/Banking_System.java
+++ src/Banking_System.java
@@ -1,14 +1,20 @@
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
-import java.io.PrintWriter;
 import java.io.File;
 public class Banking_System {
     public static void main(String[] args) throws IOException {
         File balance = new File("balance.txt");
         File transactions = new File("transactions.txt");
         Scanner info = new Scanner(balance);
-        int acctbal = info.nextInt();
+        String userResponse = null;
+        int acctbal;
+        if(info.hasNextInt()){
+            acctbal = info.nextInt();
+        }
+        else{
+            acctbal = 1000;
+        }
         info = new Scanner(transactions);
         String transString = "";
         int transNumber = 0;
@@ -16,38 +22,40 @@ public class Banking_System {
             transString += info.nextLine() + "\n";
             transNumber++;
         }
+        do {
+            System.out.println("Welcome to CIS-2348 Banking System!");
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter your Option as a number: 1. Display balance summary 2. Deposit amount 3. Withdraw amount");
+            String option = sc.nextLine();
 
-        System.out.println("Welcome to CIS-2348 Banking System!");
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your Option as a number: 1. Display balance summary 2. Deposit amount 3. Withdraw amount");
-        String option = sc.nextLine();
+            if (option.equals("1")) {
+                System.out.println("Your balance is: $" + acctbal);
+                System.out.print(transString);
+            }//if end
 
-        if (option.equals("1")) {
-            System.out.println("Your balance is: $" + acctbal);
-            System.out.print(transString);
-        }//if end
-
-        if (option.equals("2")) {
-            System.out.println("How much money would you like to deposit?");
-            int dep = sc.nextInt();// dep assigned to deposit amount
-            acctbal += dep;
-            transNumber++;
-            transString += transNumber + ". Deposit $" + dep + "  $" + acctbal;
-            System.out.println("New account balance: $" + acctbal);
-        }//if end
-
-        if (option.equals("3")) {
-            System.out.println("How much money would you like to withdraw?");
-            int wid = sc.nextInt();// wid assigned to withdraw amount
-            if (wid >= acctbal) {
-                System.out.println("Insufficient balance");
-            } else {
-                acctbal -= wid;
+            if (option.equals("2")) {
+                System.out.println("How much money would you like to deposit?");
+                int dep = sc.nextInt();// dep assigned to deposit amount
+                acctbal += dep;
                 transNumber++;
-                transString += transNumber + ". Withdrawal $" + wid + "  $" + acctbal;
+                transString += transNumber + ". Deposit $" + dep + "  $" + acctbal;
                 System.out.println("New account balance: $" + acctbal);
-            }
-        }//if end
+            }//if end
+            if (option.equals("3")) {
+                System.out.println("How much money would you like to withdraw?");
+                int wid = sc.nextInt();// wid assigned to withdraw amount
+                if (wid >= acctbal) {
+                    System.out.println("Insufficient balance");
+                } else {
+                    acctbal -= wid;
+                    transNumber++;
+                    transString += transNumber + ". Withdrawal $" + wid + "  $" + acctbal;
+                    System.out.println("New account balance: $" + acctbal);
+                }
+            }//if end
+            System.out.println("Do you want to make another transaction? Y or N");
+            if (sc.hasNext()) userResponse = sc.next();
+        } while (userResponse.equals("Y"));
         FileWriter fw = new FileWriter(balance);
         fw.write(Integer.toString(acctbal));
         fw.close();
